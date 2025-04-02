6574bcc91cd23d15a53eb8b69f78988002d6f8bf
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2025-04-02 15:50:02 +0200

    Create Carte_Bancaire.java

---
+++ Carte_Bancaire.java
@@ -0,0 +1,9 @@
+package modele;
+
+import java.util.Date;
+
+public class Carte_Bancaire {
+    private int ID_proprietaire_CB;
+    private int numero_carte_bancaire;
+    private Date date_experiration_CB; // voir la data pour date
+}

7667021288d74223ee86d5c803480dd1a6fc3985
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2025-04-19 12:56:00 +0200

    Update Ecran_payemment.java

--- Ecran_payemment.java
+++ Ecran_payemment.java
@@ -10,3 +10,13 @@ import modele.*;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+
+public class Ecran_payemment extends JFrame {
+    private DaoFactory daoFactory;
+    private Personne client;
+    private ArrayList<Commande> commandesNonPayees;
+
+    public Ecran_payemment(DaoFactory daoFactory, ArrayList<Commande> commandesNonPayees, Personne client) {
+        this.daoFactory = daoFactory;
+        this.commandesNonPayees = commandesNonPayees;
+        this.client = client;

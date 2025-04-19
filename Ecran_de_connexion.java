272d3f36d917784aed826b841691c8c3c9eef876
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2025-04-19 12:41:58 +0200

    Create Ecran_de_connexion.java

---
+++ Ecran_de_connexion.java
@@ -0,0 +1,45 @@
+package Vue;
+import Dao.DaoFactory;
+import Dao.PersonneDAOImpl;
+import Controleur.SQL_securite;
+import modele.Personne;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ecran_de_connexion extends JFrame {
+    private JTextField champUtilisateur;
+    private JPasswordField champMotDePasse;
+    private JButton boutonConnexion, boutonInscription;
+    private SQL_securite secu=new SQL_securite();
+    private Personne client=null;
+    private PersonneDAOImpl pers=null;
+
+    public Ecran_de_connexion(DaoFactory daoFactory) {
+        setTitle("Écran de Connexion");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);;
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        // Titre "S'identifier"
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        JLabel titre = new JLabel("S'identifier");
+        titre.setFont(new Font("Arial", Font.BOLD, 18));
+        add(titre, gbc);
+
+        // Champ utilisateur
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        add(new JLabel("Adresse mail:"), gbc);
+        champUtilisateur = new JTextField(15);
+        gbc.gridx = 1;
+        add(champUtilisateur, gbc);

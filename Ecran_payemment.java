40199f78fdb098c9e2163f06917eb2f18022bbe7
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2025-04-19 12:55:36 +0200

    Create Ecran_payemment.java

---
+++ Ecran_payemment.java
@@ -0,0 +1,12 @@
+package Vue;
+
+import Dao.CommandeDAOImpl;
+import Dao.DaoFactory;
+import Dao.ProduitDAOImpl;
+import modele.Commande;
+
+import modele.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;

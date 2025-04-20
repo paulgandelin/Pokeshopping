91b0bbcc6e006796158684486ebdaa5dba7b7632
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2025-04-20 20:05:27 +0200

    Delete Vue directory

--- Vue/message_reussite.java
+++
@@ -1,9 +0,0 @@
-package Vue;
-
-import javax.swing.*;
-
-public class message_reussite {
-    public void message_creation_compte_ok(){
-        JOptionPane.showMessageDialog(null, "Bienvenu \n Création du compte réussi", "Message de confirmation de la création du compte", JOptionPane.INFORMATION_MESSAGE);
-    }
-}
